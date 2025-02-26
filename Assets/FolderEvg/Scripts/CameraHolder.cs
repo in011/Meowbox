76f5509b878cb43cade8f3d8a5d98cb1a88faace
in0ii <vlasovevk@gmail.com>
2025-02-26 16:14:55 +0500

    CameraFix

--- Assets/FolderEvg/Scripts/CameraHolder.cs
+++ Assets/FolderEvg/Scripts/CameraHolder.cs
@@ -9,6 +9,7 @@ public class CameraHolder : MonoBehaviour
     public Transform secondPlayer;
     private Vector3 cameraPosition;
     public Camera camera;
+    public float splitCameraDistance = 12.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +21,13 @@ public class CameraHolder : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(Vector3.Distance(secondPlayer.position, playerObject.position));
-
-        if (Vector3.Distance(secondPlayer.position, playerObject.position) < 25f)
+        if (Vector3.Distance(secondPlayer.position, playerObject.position) < splitCameraDistance)
         {
-            Debug.Log("Single");
             camera.rect = new Rect(0f, 0f, 1f, 1f);
                 //.Set(0f, 0f, 1f, 1f);
         }
         else
         {
-            Debug.Log("Half");
             if (player1)
             {
                 camera.rect = new Rect(0f, 0f, 0.5f, 1f);
