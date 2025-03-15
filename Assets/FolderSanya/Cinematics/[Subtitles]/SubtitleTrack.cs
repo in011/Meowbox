b7cb647dcdd8a0a9f1bc31c1de7617b929ab3653
Dv1natzad48 <vaultcache@mail.ru>
2025-03-16 00:10:53 +0300

    Nado Zamenit Subtitle
    Maybe this can be good
    https://www.youtube.com/watch?v=12bfRIvqLW4

---
+++ Assets/FolderSanya/Cinematics/[Subtitles]/SubtitleTrack.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+ [TrackBindingType(typeof(TextMeshProUGUI))]
+ [TrackClipType(typeof(SubtitleClip))]
+public class SubtitleTrack : TrackAsset
+{
+    
+}
