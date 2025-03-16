9b28d158882302ee3c512328cb8929fc19460594
in0ii <vlasovevk@gmail.com>
2025-03-16 16:47:03 +0500

    Subtitle Scripts

--- Assets/FolderSanya/Cinematics/[Subtitles]/SubtitleBehaviour.cs
+++ Assets/FolderSanya/Cinematics/[Subtitles]/SubtitleBehaviour.cs
@@ -5,11 +5,4 @@ using UnityEngine.Playables;
 public class SubtitleBehaviour : PlayableBehaviour
 {
     public string subtitleText;
-
-    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
-    {
-       TextMeshProUGUI text = playerData as TextMeshProUGUI;
-       text.text = subtitleText;
-       text.color = new Color(1, 1, 1, info.weight);
-    }
 }
