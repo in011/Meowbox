869a6c73173b4e0bac31350dae192a56db5c9687
in0ii <vlasovevk@gmail.com>
2025-06-25 23:21:40 +0500

    New Dialog

--- Assets/FolderEvg/Scripts/Additional/FloatingText.cs
+++ Assets/FolderEvg/Scripts/Additional/FloatingText.cs
@@ -1,15 +1,51 @@
 using UnityEngine;
+using TMPro;
 
 public class FloatingText : MonoBehaviour
 {
+    [SerializeField] public bool LastLine = false;
     [SerializeField] public float LifeTime = 3f;
     [SerializeField] public Vector3 TextOffset = new Vector3(0,0,0);
     [SerializeField] public Vector3 RandomizeIntensity = new Vector3(0,0,0);
 
     private Animator animator;
+    private DialogMenu dialogMenu;
 
     private void Start()
     {
+        // Найти локальный TMP в себе или дочерних объектах
+        TextMeshPro localTMP = GetComponentInChildren<TextMeshPro>();
+        if (localTMP == null)
+        {
+            Debug.LogWarning("FloatingText: не найден локальный TextMeshPro!");
+            return;
+        }
+
+        string message = localTMP.text;
+        localTMP.text = "...";
+
+        // Найти объект с компонентом DialogMenu на сцене
+        dialogMenu = FindObjectOfType<DialogMenu>();
+        if (dialogMenu == null)
+        {
+            Debug.LogWarning("FloatingText: не найден объект с DialogMenu на сцене!");
+            return;
+        }
+        dialogMenu.WakeUp();
+
+        dialogMenu.dialogTMP.text = message;
+        /*
+        // Найти TextMeshProUGUI (UI-версия текста)
+        TextMeshProUGUI dialogText = dialogMenu.GetComponentInChildren<TextMeshProUGUI>();
+        if (dialogText == null)
+        {
+            Debug.LogWarning("FloatingText: не найден TextMeshProUGUI в DialogMenu!");
+            return;
+        }
+
+        // Перенести текст
+        dialogText.text = message;*/
+
         gameObject.TryGetComponent<Animator>(out animator);
         transform.localPosition += TextOffset;
 
@@ -24,6 +60,23 @@ public class FloatingText : MonoBehaviour
             Invoke(nameof(Close), LifeTime-1f);
         }
     }
+
+    private void OnDestroy()
+    {
+        if (LastLine)
+        {
+            if (dialogMenu == null)
+            {
+                Debug.LogWarning("No dialogMenu to turn off!");
+                return;
+            }
+            else
+            {
+                dialogMenu.Sleep();
+            }
+        }
+    }
+
     private void Close()
     {
         animator.SetTrigger("Close");
